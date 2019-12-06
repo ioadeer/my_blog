292ea73304be35af69b09b4de275ca34f9eb0ebb
Jim Reed <cjoackin@gmail.com>
2019-12-06 00:53:54 -0300

    Added files and changes required for deployment to heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,13 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's6ns5gx4d)rt0%-4b1g18(1c32cm#yee^n%l8t6-x+z(%#do=('
+# SECRET_KEY = 's6ns5gx4d)rt0%-4b1g18(1c32cm#yee^n%l8t6-x+z(%#do=('
+import os 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 's6ns5gx4d)rt0%-4b1g18(1c32cm#yee^n%l8t6-x+z(%#do=(')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['my-django-postgres-blog','127.0.0.1','0.0.0.0']
 
 # Application definition
 
@@ -42,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,9 +123,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
