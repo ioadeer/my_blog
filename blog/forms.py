8815fb6465150686b7d151f1d83d6dd543fca6ce
Jim Reed <cjoackin@gmail.com>
2019-12-16 17:57:23 -0300

    Nuevo commit para modificar cuestiones de debug

--- blog/forms.py
+++ blog/forms.py
@@ -7,4 +7,5 @@ from django.utils.translation import ugettext_lazy as _
 class CreatePostForm(forms.Form):
     title = forms.CharField(max_length=100, help_text="Write the title here.")
     text = forms.CharField(help_text="Write article here",widget = forms.Textarea)
+
     categories = forms.ModelMultipleChoiceField(queryset=Category.objects.all())
--- myproject/settings.py
+++ myproject/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 import dj_database_url
