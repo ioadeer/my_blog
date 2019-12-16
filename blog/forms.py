e77f78b0229d68c71bf1b4317b55758657d32d23
Jim Reed <cjoackin@gmail.com>
2019-12-16 17:31:58 -0300

    Added new form for post creation, list of categories on post detail

--- blog/forms.py
+++ blog/forms.py
@@ -6,5 +6,5 @@ from django.utils.translation import ugettext_lazy as _
 
 class CreatePostForm(forms.Form):
     title = forms.CharField(max_length=100, help_text="Write the title here.")
-    text = forms.CharField(help_text="Write article here")
+    text = forms.CharField(help_text="Write article here",widget = forms.Textarea)
     categories = forms.ModelMultipleChoiceField(queryset=Category.objects.all())
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post
+from blog.models import Post, Category
 # Create your views here.
 
 def index(request):
    return render(request,'index.html')
 
 from django.views import generic 
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse_lazy
 
 class PostListView(generic.ListView):
     model = Post
-    paginate_by = 10
+    paginate_by = 50
 
 class PostDetailView(generic.DetailView):
     model = Post
@@ -27,24 +30,36 @@ class PostUpdate(UpdateView):
     model = Post
     fields = ['title', 'text', 'categories']
 
+class PostDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'blog.delete_post'
+    model = Post
+    success_url = reverse_lazy('all-posts')
+
 from blog.forms import CreatePostForm 
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
+from pdb import set_trace
 
 @permission_required('blog.create_post')
 def create_post(request):
     """ Create post view for authorized users """
+    # no esta andando categories
     if request.method == "POST":
         form =  CreatePostForm(request.POST)
         if form.is_valid():
             #data = forms.cleaned_data
             data = {'title': form.cleaned_data['title']}
-            print(data)
-            data['author'] = request.user
-            print(data)
+            user_name = request.user.username
+            user = User.objects.filter(username__exact=user_name).first()
+            data['author'] = user
+            data['text'] = form.cleaned_data['text']
             post = Post.objects.create(**data)
+            category_queryset = form.cleaned_data['categories']
+            #print(category_queryset)
+            post.categories.set(category_queryset)
+            set_trace()
             post.save()
             return HttpResponseRedirect(reverse('all-posts'))
     else:
