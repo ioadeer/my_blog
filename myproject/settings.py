14c9b614719411785cf5b0d8fe9974050856f0b8
Jim Reed <cjoackin@gmail.com>
2020-07-29 20:35:05 -0300

    Testing S3 bucket

--- myproject/settings.py
+++ myproject/settings.py
@@ -145,7 +145,8 @@ DATABASES['default'].update(db_from_env)
 #import django_heroku
 django_heroku.settings(locals())
 
-USE_S3 = os.getenv('USE_S3') == 'TRUE'
+#USE_S3 = os.getenv('USE_S3') == 'TRUE'
+USE_S3 = True
 
 if USE_S3:
     # aws settings
